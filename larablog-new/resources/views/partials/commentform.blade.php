b62f4b833e7ab69172defd9eed2b43e9ef333314
ashokdev <jobeditions@gmail.com>
2017-05-18 14:39:54 +0200

    18 May Commit before the Comment Controller

--- larablog-new/resources/views/partials/commentform.blade.php
+++ larablog-new/resources/views/partials/commentform.blade.php
@@ -2,7 +2,7 @@
 			  <div class="leave">
 				<h4>Leave a comment</h4>
 			  </div>
-				<form id="commentform">
+				<form id="commentform" method="POST" action="/posts/{{$post->id}}/comments">
 				    <label for="author">Name</label>
 						<input id="author" name="author" type="text" value="" size="30" aria-required="true">
 						<label for="email">Email</label>
@@ -10,7 +10,7 @@
 						<label for="url">Website</label>
 						<input id="url" name="url" type="text" value="" size="30">
 						<label for="comment">Comment</label>
-						<textarea></textarea>
+						<textarea name="body" placeholder="Vos commentaires ici"></textarea>
 					 <div class="clearfix"></div>
 			           <input name="submit" type="submit" id="submit" value="Send">
 					<div class="clearfix"></div>
